b9d5c91826439646829acc1d846724999e34ab26
xiaozhang <xiaozhang@freewheel.tv>
2017-07-16 21:46:17 +0800

    update doc scripts

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,8 +60,8 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'Turbo Doc'
-copyright = u'2017, xiaozhang'
-author = u'xiaozhang'
+copyright = u'2017, Xiao Zhang'
+author = u'Xiao Zhang'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -236,7 +236,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'turbodoc.tex', u'turbodoc Documentation',
-     u'xiaozhang', 'manual'),
+     u'Xiao Zhang', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
